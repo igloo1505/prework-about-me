1d6ae5f449d67daceccb088c8ce2730a07b2c844
Andrew Mueller <iglooworks1505@gmail.com>
2019-11-28 23:32:36 -0600

    added opacity transition to footer, and cleared up some unneeded consolelogs

--- js/index.js
+++ js/index.js
@@ -6,8 +6,27 @@ let offSet = (h / 2) - (16 * 4);
 let offSetpx = `${offSet}px`;
 document.getElementById('wrap').style.top = offSetpx;
 
+
+// Cheesy Fade-in/out effects
 document.onscroll = () =>  {
 var scrollTop  = window.pageYOffset;
-var opac = (h - scrollTop) / h;
-document.getElementById('masthead').style.opacity = opac;
+var opacHeader = (h - scrollTop) / h;
+document.getElementById('masthead').style.opacity = opacHeader;
+
+
+var winHeight = window.innerHeight;
+var footHeight = document.getElementById('footer').offsetHeight;
+var opacFooter = scrollTop + winHeight + footHeight;
+var showFooter = opacFooter - document.getElementById('body').clientHeight;
+
+
+var footer = document.getElementById('footer');
+if (showFooter > 60) {   
+    footer.style.opacity = 1;
+    footer.style.transition = 'opacity 1s'
+} 
+else {
+    footer.style.opacity = 0;
+    footer.style.transition = 'opacity .5s'
+}
 }
